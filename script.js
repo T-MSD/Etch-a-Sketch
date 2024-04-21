0afc469088b210c9c0802de1625b142a75046c93
T-MSD <dias.tiago03@gmail.com>
2024-04-21 14:46:15 +0100

    fixed grid dimensions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gridContainer = document.getElementById('grid-container');
 function createGrid(){
   for (let row = 0; row < 16; row++){
     const cellRow = document.createElement('div');
+    cellRow.classList.add('cell-row');
     for (let column = 0; column < 16; column++){
       const cell = document.createElement('div');
       cell.classList.add('grid-cell');
