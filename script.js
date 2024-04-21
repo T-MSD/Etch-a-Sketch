51ab0f90b18c24da410e41305f4c2b131e5eb7ce
T-MSD <dias.tiago03@gmail.com>
2024-04-21 14:20:26 +0100

    created 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+const gridContainer = document.getElementById('grid-container');
+
+function createGrid(){
+  for (let row = 0; row < 16; row++){
+    const cellRow = document.createElement('div');
+    for (let column = 0; column < 16; column++){
+      const cell = document.createElement('div');
+      cell.classList.add('grid-cell');
+      cellRow.appendChild(cell);
+    }
+    gridContainer.appendChild(cellRow);
+  }
+}
+
+createGrid();
