7a14772bc711785b91056f0b04e234f2296e7eda
T-MSD <dias.tiago03@gmail.com>
2024-04-28 19:34:43 +0100

    feat: implement hovering function, new grid button and delete grid function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const gridContainer = document.getElementById('grid-container');
+const gridContainer = document.getElementById('grid-container'); 
+const gridButton = document.getElementById('grid-button')
+
+gridButton.addEventListener('click', () => deleteGrid());
 
 function createGrid(){
   for (let row = 0; row < 16; row++){
@@ -7,10 +10,20 @@ function createGrid(){
     for (let column = 0; column < 16; column++){
       const cell = document.createElement('div');
       cell.classList.add('grid-cell');
+      cell.addEventListener('mouseenter', () => changeBackgroundColor(cell)); 
       cellRow.appendChild(cell);
     }
     gridContainer.appendChild(cellRow);
   }
 }
 
+function changeBackgroundColor(cell){
+  cell.classList.add('grid-cell-hover');
+}
+
+function deleteGrid(){
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
 createGrid();
